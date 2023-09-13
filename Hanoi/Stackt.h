75b21ae3aa0c1114fa3511f08ea83665df80c7c6
reffobic <133569987+reffobic@users.noreply.github.com>
2023-09-14 02:04:18 +0300

    Added comments

--- Hanoi/Stackt.h
+++ Hanoi/Stackt.h
@@ -14,49 +14,38 @@ using namespace std;
 class Stackt
 {
 private:
-    int top, MaxSize; //index to the top of the tower
-    int *theStack; //Stack
+    int top, MaxSize; // Index to the top of the tower and maximum size of the stack
+    int *theStack; // Pointer to the dynamic array representing the stack
     
 public:
     Stackt (int nelements){
-        MaxSize = nelements;
-        theStack = new int[MaxSize];
-        top = -1;
+        MaxSize = nelements; // Set the maximum size of the stack
+        theStack = new int[MaxSize]; // Dynamically allocate memory for the stack
+        top = -1; // Initialize top to -1 (empty stack)
     }; // Constructor
     
     ~Stackt(){
-        delete[] theStack;
+        delete[] theStack; // Destructor to free allocated memory when the object is destroyed
     }; // Destructor
     
     void Push (int); //pushes a value in the stack
     int pop (); //removes the top value of the stack
-    int stackTop() const;
     void display(int disk, char source, char target); //function to display the steps
 };
 
-int Stackt :: stackTop() const
-{
-    int v;
-    v = theStack[top];
-    return v;
-}
-
 void Stackt :: Push(int num){
-    top++;
-    theStack [top] = num;
+    top++; // Increment top to prepare for the push
+    theStack [top] = num; // Assign the value to the top of the stack
 }
-
 
 int Stackt :: pop(){
-    int popped = theStack[top];
-    top--;
-//    top--;
-    return popped;
+    int popped = theStack[top]; // Get the value at the top of the stack
+    top--; // Decrement top to remove the top element
+    return popped; // Return the value that was popped
 }
 
-
 void Stackt :: display(int disk, char source, char target){
-    cout << "Moved disk " << disk << " from " << source << " to " << target << endl;
+    cout << "Moved disk " << disk << " from " << source << " to " << target << endl; // Display the movement of a disk from source to target
 }
 
 #endif /* Stackt_h */
--- Hanoi/main.cpp
+++ Hanoi/main.cpp
@@ -10,29 +10,29 @@
 
 using namespace std;
 
-int moves = 0;
+int moves = 0; // Variable to keep track of the number of moves
 
-
+// Recursive function to solve the Tower of Hanoi problem
 void Towers (int N, Stackt& A , Stackt& B , Stackt& C, int & moves, char SourceTag, char TargetTag, char AuxTag) {
     
-    if (N == 1) {
+    if (N == 1) { // If there's only one disk, move it directly from source to target
 
-        int disk = A.pop();
-        C.display(disk, SourceTag, TargetTag);
-        C.Push(disk);
-        moves++;
+        int disk = A.pop(); // Pop a disk from the source stack
+        C.display(disk, SourceTag, TargetTag); // Display the move
+        C.Push(disk); // Push the disk to the target stack
+        moves++; // Increment the move counter
 
     }
-    else{
+    else{ // If there are more than one disk, use recursion
 
-        Towers(N - 1, A, B, C, moves, SourceTag, AuxTag, TargetTag);
+        Towers(N - 1, A, B, C, moves, SourceTag, AuxTag, TargetTag); // Move N-1 disks from source to auxiliary via target
 
-        int disk = A.pop();
-        C.display(disk, SourceTag, TargetTag);
-        C.Push(disk);
-        moves++;
+        int disk = A.pop(); // Move the remaining disk from source to target
+        C.display(disk, SourceTag, TargetTag); // Display the move
+        C.Push(disk); // Push the disk to the target stack
+        moves++; // Increment the move counter
 
-        Towers(N - 1, B, C, A, moves, AuxTag, TargetTag, SourceTag);
+        Towers(N - 1, B, C, A, moves, AuxTag, TargetTag, SourceTag); // Move N-1 disks from auxiliary to target via source
     }
 }
 
@@ -46,18 +46,18 @@ int main(){
 
     char sourceTag = 'A', auxiliaryTag = 'B', targetTag = 'C';
     
-    Stackt Source(num);
-    Stackt Aux(num);
-    Stackt Target(num);
+    Stackt Source(num); // Create a stack for the source tower
+    Stackt Aux(num); // Create a stack for the auxiliary tower
+    Stackt Target(num); // Create a stack for the target tower
     
     for (int i = 1; i<=num; i++){
         Source.Push(i);  //to insert the number of disks in the source tower
-    }
+    } // Initialize the source tower with disks numbered from 1 to num
 
-    cout << endl;
+    cout << endl; // Call the Towers function to solve the Tower of Hanoi problem
     Towers(num, Source, Aux, Target, moves, sourceTag, targetTag, auxiliaryTag);
     cout << endl;
 
-    cout << "Total moves: " << moves << endl;
+    cout << "Total moves: " << moves << endl; // Display the total number of moves
     return 0;
 }
